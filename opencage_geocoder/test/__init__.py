35063469ee760b3c19b80cb2ce978f6f81953cbf
marc tobias <mtmail@gmx.net>
2026-05-28 14:08:48 +0200

    test suite: silence -app not initialized- warning

--- opencage_geocoder/test/__init__.py
+++ opencage_geocoder/test/__init__.py
@@ -1,2 +1,10 @@
 # import qgis libs so that ve set the correct sip api version
 import qgis   # pylint: disable=W0611  # NOQA
+
+from qgis.core import QgsApplication
+
+# Initialise a headless QGIS application so the tests have a valid application
+# path (silences "Application path not initialized"). The prefix is auto-detected.
+if QgsApplication.instance() is None:
+    _qgs_app = QgsApplication([], False)
+    _qgs_app.initQgis()
