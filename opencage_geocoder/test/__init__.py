021176b58ef0243138edea37d474ca823c8efbc9
marc tobias <mtmail@gmx.net>
2026-05-28 14:22:03 +0200

    test suite: ignore temporary files

--- opencage_geocoder/test/__init__.py
+++ opencage_geocoder/test/__init__.py
@@ -1,4 +1,7 @@
 # import qgis libs so that ve set the correct sip api version
+import os
+import tempfile
+
 import qgis   # pylint: disable=W0611  # NOQA
 
 from qgis.core import QgsApplication
@@ -6,5 +9,9 @@ from qgis.core import QgsApplication
 # Initialise a headless QGIS application so the tests have a valid application
 # path (silences "Application path not initialized"). The prefix is auto-detected.
 if QgsApplication.instance() is None:
+    # Write the QGIS profile (symbology-style.db, etc.) to a temp dir so test
+    # runs don't litter the working tree.
+    os.environ.setdefault("QGIS_CUSTOM_CONFIG_PATH",
+                          tempfile.mkdtemp(prefix="qgis-test-"))
     _qgs_app = QgsApplication([], False)
     _qgs_app.initQgis()
